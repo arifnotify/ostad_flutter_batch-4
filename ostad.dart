635ae4f947091c7d030b70155f4cf7b95873c3f8
Arif <90087547+arifnotify@users.noreply.github.com>
2023-08-04 04:48:35 +0200

    Add files via upload

---
+++ ostad.dart
@@ -0,0 +1,15 @@
+
+void main(){
+
+var a = 7,b = 3;
+  var addition= a+b;
+  var subtraction =a-b;
+  var multiplication = a*b;
+  double division = a/b;
+  int modulus = a%b;
+  print (addition);
+  print (subtraction);
+  print (multiplication);
+  print (division);
+  print (modulus);
+}
